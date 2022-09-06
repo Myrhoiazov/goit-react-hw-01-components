130aa089ba2dbebfd5d165e72ccaedf364c25a83
Myrhoiazov <denis.mirgoyazov@gmail.com>
2022-09-06 17:24:16 +0300

    add: 2 homework

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
+import user from '../data/profile.json';
+import Profile from './Profile.jsx';
+
+import statistics from '../data/statistics.json';
+import Statistics from './Statistics.jsx';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <Profile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+        <Statistics title="Upload stats" stats={statistics} />
+      </div>
+    </>
   );
 };
